98513096d05a4fe9392386d296d0778a63e91f78
Oliver-Rodry <124855827+Oliver-Rodry@users.noreply.github.com>
2026-02-22 14:04:56 -0400

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -1,1 +1,21 @@
+async function loadCatalog() {
+const res = await fetch("products.csv");
+const text = await res.text();
+const rows = text.split("\n").slice(1);
 
+const container = document.getElementById("catalog");
+
+rows.forEach(row => {
+if(!row) return;
+
+const cols = row.split(",");
+const name = cols[1];
+const price = cols[3];
+
+const div = document.createElement("div");
+div.innerHTML = `<strong>${name}</strong><br>Precio: RD$ ${price}`;
+container.appendChild(div);
+});
+}
+
+loadCatalog();
